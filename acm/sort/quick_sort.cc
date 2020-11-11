ff41512f08a642d096f63eb64578b9ec761f01ea
lioncfliu <lioncfliu@tencent.com>
2020-11-11 09:46:38 +0800

    feat(sort): add quick sort

--- acm/sort/quick_sort.cc
+++ acm/sort/quick_sort.cc
@@ -4,35 +4,30 @@
 
 using sort_function = std::function<void(int * /* array */, int /* left */, int /* right */)>;
 
-int get_middle_index(int *array, int left, int right) {
+int partition(int *array, int left, int right) {
   int middle = array[left];
-  int l = left, r = right - 1;
+  int l = left;
+  int r = right;
+
   while (l < r) {
-    while (l < r && array[r] > middle) --r; 
-    if (l < r) {
-      array[l] = array[r];
-      ++l;
-    }
-   
-    while (l < r && array[l] < middle) ++l;
-    if (l < r) {
-      array[r] = array[l];
-      --r;
-    }
-    std::cout<< "middle index, l, r:" << l << "," << r << std::endl;
+     while (l < r && array[r] >= middle) --r;
+     array[l] = array[r];
+
+     while (l < r && array[l] <= middle) ++l;
+     array[r] = array[l];
   }
   array[l] = middle;
   return l;
 }
 
 // Time cost: O(n log2 n)
-// Space cost: O(n log2 n)
+// Space cost: O(1) 
 void quick_sort(int *array, int left, int right) {
-  if (left > right) return;
+  if (left >= right) return;
   
-  int middle_index = get_middle_index(array, left, right);
-  quick_sort(array, left, middle_index - 1); 
-  quick_sort(array, middle_index + 1, right); 
+  int pivot = partition(array, left, right);
+  quick_sort(array, left, pivot - 1); 
+  quick_sort(array, pivot + 1, right); 
 }
 
 void display_array(int *array, int left, int right) {
@@ -47,14 +42,17 @@ void run_all_testings() {
   std::vector<int> array_01{9, 8, 7, 6, 5, 4, 3, 2, 1};
   std::vector<int> array_02{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1};
   std::vector<int> array_03{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<int> array_04{9, 8, 9, 7, 5, 6 , 3, 2, 1, 2};
   
-  sort_int_array(array_01.data(), 0, array_01.size());
-  sort_int_array(array_02.data(), 0, array_02.size());
-  sort_int_array(array_03.data(), 0, array_03.size());
+  sort_int_array(array_01.data(), 0, array_01.size() - 1);
+  sort_int_array(array_02.data(), 0, array_02.size() - 1);
+  sort_int_array(array_03.data(), 0, array_03.size() - 1);
+  sort_int_array(array_04.data(), 0, array_04.size() - 1);
 
   display_array(array_01.data(), 0, array_01.size());
   display_array(array_02.data(), 0, array_02.size());
   display_array(array_03.data(), 0, array_03.size());
+  display_array(array_04.data(), 0, array_04.size());
 }
 
 int main(int argc, char *argv[]) {
